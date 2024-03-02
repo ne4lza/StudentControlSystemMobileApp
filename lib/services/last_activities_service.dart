a1e0953ecffc0b57ef7998978a7218dfa69a3b71
enes.kilic <ekilic1267@gmail.com>
2024-03-02 16:31:47 +0300

    added qr service

--- lib/services/last_activities_service.dart
+++ lib/services/last_activities_service.dart
@@ -3,27 +3,25 @@ import 'dart:convert';
 import 'package:get_storage/get_storage.dart';
 import 'package:kolej_client/models/last_activities_model.dart';
 import 'package:http/http.dart' as http;
+
 class LastActivitiesService {
-    Future<List<LastActivitiesModel>> getLastActivities() async {
-  final String token = await GetStorage().read("token");
-  final String url = "products";
-  var response = await http.get(
-    Uri.parse(url),
-    headers: {
+  Future<List<LastActivitiesModel>> getLastActivities() async {
+    final String token = await GetStorage().read("token");
+    final String url = "products";
+    var response = await http.get(Uri.parse(url), headers: {
       'Authorization': 'Bearer $token',
-    }
-  );
+    });
 
     List<LastActivitiesModel> lastActivities = [];
     var responseList = jsonDecode(response.body)['products'];
     print(response.body);
-  if (response.statusCode == 200) {
-    for (var jsondata in responseList) {
-      lastActivities.add(LastActivitiesModel.fromJson(jsondata));
+    if (response.statusCode == 200) {
+      for (var jsondata in responseList) {
+        lastActivities.add(LastActivitiesModel.fromJson(jsondata));
+      }
+      return lastActivities;
     }
-    return lastActivities;
-  }
 
-  return List.empty();
-}
+    return List.empty();
+  }
 }
--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -4,30 +4,31 @@ import 'package:get_storage/get_storage.dart';
 import 'package:kolej_client/constants/constants.dart';
 import 'package:kolej_client/models/login_model.dart';
 import 'package:http/http.dart' as http;
-class LoginService {
 
+class LoginService {
   Future<LoginModel?> loginUser(
       {required String username, required String password}) async {
     final tokenBox = GetStorage();
     final userNameBox = GetStorage();
     final String url = "${Constants.testBaseUrl}Giris/Token";
-    Map<String, dynamic> jsonModel = {"UserName": username, "Password": password};
+    Map<String, dynamic> jsonModel = {
+      "UserName": username,
+      "Password": password
+    };
     final response = await http.post(Uri.parse(url),
-    headers: {'Content-Type': 'application/json'}, 
-    body: jsonEncode(jsonModel)
-    );
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(jsonModel));
 
     if (response.statusCode == 200) {
       print("Başarılı: ${response.body}");
       var result = LoginModel.fromJson(jsonDecode(response.body));
       print("Gelen Response => $result");
       tokenBox.write("token", result.token);
-      userNameBox.write("userName",username);
+      userNameBox.write("userName", username);
       print(tokenBox.read("token"));
       print(tokenBox.read("userName"));
       return result;
     }
-    return null; 
+    return null;
   }
-  
 }
